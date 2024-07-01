2e365383b10bd33a4fc4b9082935dcd4f8a14b0b
Sho Kaneko <kanekoshoyu@gmail.com>
2024-07-01 16:39:58 +0800

    model dir update

--- generate_model.py
+++ generate_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import asyncio
 import os
 import re
@@ -7,6 +8,12 @@ from enum import Enum
 class Parser(Enum):
     ASYNC_API = 'asyncapi'
     OPEN_API = 'openapi'
+
+
+class Language(Enum):
+    PYTHON = 'python'
+    RUST = 'rust'
+
 
 
 def remove_suffix(text, suffix):
@@ -19,12 +26,13 @@ async def generate_rust_models(yaml_file, parser):
     # for testing
     # output_dir = "output/"
     # for production
-    output_dir = "target/"
-    modes = ["python", "rust"]
+    languages = [Language.PYTHON, Language.RUST]
     if parser == Parser.ASYNC_API:
-        output_dir_name = remove_suffix(yaml_file, '_asyncapi.yml')
-        for mode in modes:
-            command = f"asyncapi generate models {mode} {yaml_file} -o {output_dir}/{mode}/{output_dir_name}"
+        output = remove_suffix(yaml_file, '_asyncapi.yml')
+        for language in languages:
+            output_dir = f"target/{language.value}/{output}/model"
+            command = f"asyncapi generate models {language.value} {yaml_file} -o {output_dir}"
+            print(f"command: {command}")
             proc = await asyncio.create_subprocess_shell(command)
             await proc.communicate()
     elif parser == Parser.OPEN_API:
@@ -32,7 +40,6 @@ async def generate_rust_models(yaml_file, parser):
         print("todo implement open API parser")
     else:
         raise ValueError(f"Unknown parser type: {parser}")
-
 
 
 async def process_yaml_files(yaml_directory):
