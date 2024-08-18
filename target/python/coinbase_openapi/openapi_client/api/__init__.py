132a0e1f349118aedb2bfbbe42a7e5090b62908a
Sho Kaneko <kanekoshoyu@gmail.com>
2024-08-19 01:43:46 +0800

    generate model for hyperliquid and coinbase

---
+++ target/python/coinbase_openapi/openapi_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.accounts_api import AccountsApi
+from openapi_client.api.addresses_api import AddressesApi
+from openapi_client.api.transactions_api import TransactionsApi
+from openapi_client.api.users_api import UsersApi
+
---
+++ target/python/hyperliquid_openapi/openapi_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.exchange_api import ExchangeApi
+from openapi_client.api.info_api import InfoApi
+from openapi_client.api.websocket_api import WebsocketApi
+
