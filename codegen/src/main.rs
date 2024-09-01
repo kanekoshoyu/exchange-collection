a3d9767bd595ad28383bb626dd34e2166a9b9358
Sho Kaneko <kanekoshoyu@gmail.com>
2024-09-01 10:58:58 +0800

    change to use AsPath (some errors now)

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,25 +1,30 @@
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use strum::IntoEnumIterator;
-use strum_macros::{EnumIter, EnumString};
+use strum_macros::{Display, EnumIter, EnumString};
 
 #[derive(Parser, Debug)]
 struct CliInput {
     #[arg(short, long)]
     /// Some when batch
-    input_directory: Option<String>,
+    input_directory: Option<PathBuf>,
     /// None when batch, Some when we convert one file only
-    input_filename: Option<String>,
-    output_directory: Option<String>,
+    input_filename: Option<PathBuf>,
+    output_directory: Option<PathBuf>,
     // None when batch, Some when select one
     output_language: Vec<ProgrammingLanguage>,
 }
 impl Default for CliInput {
     fn default() -> Self {
         Self {
-            input_directory: Some("asset".to_string()),
+            input_directory: Some(PathBuf::from_str("asset").unwrap()),
             input_filename: None,
-            output_directory: Some("target".to_string()),
+            output_directory: Some(PathBuf::from_str("target").unwrap()),
             output_language: ProgrammingLanguage::iter().collect(),
         }
     }
@@ -47,30 +52,67 @@ impl CliInput {
 }
 
 #[derive(
-    Clone, Copy, Debug, Deserialize, Serialize, EnumIter, EnumString, PartialEq, PartialOrd, Eq, Ord,
+    Clone,
+    Copy,
+    Debug,
+    Display,
+    Deserialize,
+    Serialize,
+    EnumIter,
+    EnumString,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Ord,
 )]
-#[serde(rename_all = "lowercase")]
 enum ProgrammingLanguage {
+    #[strum(serialize = "rust")]
     Rust,
+    #[strum(serialize = "python")]
     Python,
 }
 
 #[derive(
-    Clone, Copy, Debug, Deserialize, Serialize, EnumIter, EnumString, PartialEq, PartialOrd, Eq, Ord,
+    Clone,
+    Copy,
+    Debug,
+    Display,
+    Deserialize,
+    Serialize,
+    EnumIter,
+    EnumString,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Ord,
 )]
-#[serde(rename_all = "lowercase")]
 enum ApiFileFormat {
+    #[strum(serialize = "openapi")]
     OpenApi,
+    #[strum(serialize = "asyncapi")]
     AsyncApi,
 }
 
 #[derive(
-    Clone, Copy, Debug, Deserialize, Serialize, EnumIter, EnumString, PartialEq, PartialOrd, Eq, Ord,
+    Clone,
+    Copy,
+    Debug,
+    Display,
+    Deserialize,
+    Serialize,
+    EnumIter,
+    EnumString,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Ord,
 )]
-#[serde(rename_all = "lowercase")]
 enum Protocol {
+    #[strum(serialize = "rest")]
     Rest,
+    #[strum(serialize = "ws")]
     Ws,
+    #[strum(serialize = "fix")]
     Fix,
 }
 
@@ -81,21 +123,38 @@ struct InputFileParameter {
     format: ApiFileFormat,
 }
 impl InputFileParameter {
-    pub fn from_filename(filename: &str) -> Result<Self, ()> {
-        // "binance_ws_asyncapi.yaml"
-        if !filename.ends_with(".yaml") {
+    pub fn from_filename(filename: impl AsRef<Path>) -> Result<Self, ()> {
+        let filename = filename.as_ref();
+        if !filename.is_file() {
             return Err(());
         }
+        // "assets/binance_ws_asyncapi.yaml"
+        if !filename.ends_with(".yaml") {
+            println!("invalid format");
+            return Err(());
+        }
+
+        // "binance_ws_asyncapi.yaml"
+        let filename = filename.file_name().unwrap();
 
         // "binance_ws_asyncapi"
-        let rest = filename.replace(".yaml", "");
+        let rest = format!("{:?}", filename).replace(".yaml", "");
 
         // "binance", "ws", "asyncapi"
-        let str_vec: Vec<&str> = rest.split_inclusive("_").into_iter().collect();
+        let str_vec: Vec<&str> = rest.split("_").collect();
+
         if str_vec.len() != 3 {
+            println!("invalid format");
             return Err(());
         }
 
+        println!("str_vec: {:?}", str_vec);
+
+        // Ok(InputFileParameter {
+        //     exchange: str_vec[0].to_string(),
+        //     protocol: str_vec[1].parse::<Protocol>().map_err(|_e| ())?,
+        //     format: str_vec[2].parse::<ApiFileFormat>().map_err(|_e| ())?,
+        // })
         Ok(InputFileParameter {
             exchange: str_vec[0].to_string(),
             protocol: str_vec[1].parse::<Protocol>().map_err(|_e| ())?,
@@ -118,7 +177,9 @@ fn run() -> Result<(), ()> {
             let mut filenames = Vec::new();
             for file in files {
                 let file = file.map_err(|_| ())?;
-                filenames.push(format!("{}/{:?}", input_dir, file.file_name()));
+                let mut filename = input_dir.clone();
+                filename.push(file.file_name());
+                filenames.push(filename);
             }
 
             let output_directory = cli.output_directory.unwrap();
@@ -135,8 +196,9 @@ fn run() -> Result<(), ()> {
         }
         (Some(input_filename), None) => {
             // single load
+
             let output_directory = cli.output_directory.unwrap();
-            for output_language in cli.output_language {
+            for output_language in cli.output_language.clone() {
                 let command = codegen_command_str(
                     input_filename.clone(),
                     output_directory.clone(),
@@ -151,8 +213,8 @@ fn run() -> Result<(), ()> {
 }
 
 fn codegen_command_str(
-    input_filename: String,
-    output_directory: String,
+    input_filename: PathBuf,
+    output_directory: PathBuf,
     language: ProgrammingLanguage,
 ) -> Result<String, ()> {
     // openapi-generator-cli generate -i example_openapi.yaml -g <language> -o output/example_rust_model
@@ -161,33 +223,35 @@ fn codegen_command_str(
     let exchange = param.exchange;
     let protocol = param.protocol;
     let format = param.format;
+    let output_directory = match language {
+        ProgrammingLanguage::Rust => format!(
+            "{}/{}/{}/src/{}",
+            output_directory.display(),
+            language,
+            exchange,
+            protocol
+        ),
+        ProgrammingLanguage::Python => format!(
+            "{}/{}/{}/{}",
+            output_directory.display(),
+            language,
+            exchange,
+            protocol
+        ),
+    };
+    let output_directory = Path::new(&output_directory);
     Ok(match format {
-        ApiFileFormat::OpenApi => {
-            let output_directory = match language {
-                ProgrammingLanguage::Rust => format!(
-                    "{}/{:?}/{}/src/{:?}",
-                    output_directory, language, exchange, protocol
-                ),
-                ProgrammingLanguage::Python => format!(
-                    "{}/{:?}/{}/{:?}",
-                    output_directory, language, exchange, protocol
-                ),
-            };
-            format!("openapi-generator-cli generate -g {language:?} -i {input_filename} -o {output_directory}")
-        }
-        ApiFileFormat::AsyncApi => {
-            let output_directory = match language {
-                ProgrammingLanguage::Rust => format!(
-                    "{}/{:?}/{}/src/{:?}",
-                    output_directory, language, exchange, protocol
-                ),
-                ProgrammingLanguage::Python => format!(
-                    "{}/{:?}/{}/{:?}",
-                    output_directory, language, exchange, protocol
-                ),
-            };
-            format!("asyncapi generate models {language:?} {input_filename} -o {output_directory}")
-        }
+        ApiFileFormat::OpenApi => format!(
+            "openapi-generator-cli generate -g {language} -i {} -o {}",
+            input_filename.display(),
+            output_directory.display()
+        ),
+
+        ApiFileFormat::AsyncApi => format!(
+            "asyncapi generate models {language} {} -o {}",
+            input_filename.display(),
+            output_directory.display()
+        ),
     })
 }
 
@@ -200,11 +264,13 @@ pub fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
     #[test]
     fn test_codegen_command_str() {
         use super::*;
-        let input_filename = "binance_ws_asyncapi.yaml".to_string();
-        let output_directory = "./".to_string();
+        let input_filename = PathBuf::from_str("binance_ws_asyncapi.yaml").unwrap();
+        let output_directory = PathBuf::from_str("./").unwrap();
         let output_language = ProgrammingLanguage::Rust;
         let command = match codegen_command_str(input_filename, output_directory, output_language) {
             Ok(command) => command,
@@ -217,11 +283,39 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_language() {
+    fn test_deserialize_language() {
         use super::*;
-        match "python".parse::<ProgrammingLanguage>() {
+        match ProgrammingLanguage::from_str("python") {
             Ok(language) => assert_eq!(language, ProgrammingLanguage::Python),
             Err(e) => panic!("{}", e),
         }
+    }
+
+    #[test]
+    fn test_serialize_language() {
+        use super::*;
+        let language = ProgrammingLanguage::Python;
+        assert_eq!(format!("{language}"), "python");
+    }
+
+    #[test]
+    fn test_parse_input_file() {
+        use super::*;
+        match InputFileParameter::from_filename("../asset/binance_ws_asyncapi.yaml") {
+            Ok(parameter) => {
+                assert_eq!(parameter.exchange, "binance");
+                assert_eq!(parameter.protocol, Protocol::Ws);
+                assert_eq!(parameter.format, ApiFileFormat::AsyncApi);
+            }
+            Err(e) => panic!("{:?}", e),
+        }
+    }
+
+    #[test]
+    fn test_path() {
+        let str_path = "../asset/binance_ws_asyncapi.yaml";
+        let path = PathBuf::from_str(str_path).unwrap();
+        let canonical = path.canonicalize().unwrap();
+        assert!(canonical.is_file());
     }
 }
