b5a77724cc121158da9359259a087cc9193d934e
Sho Kaneko <kanekoshoyu@gmail.com>
2024-09-01 12:15:57 +0800

    remove old definition

--- target/rust/binance_asyncapi/model/order_type.rs
+++
@@ -1,8 +0,0 @@
-// OrderType represents a OrderType model.
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-pub enum OrderType {
-    #[serde(rename="LIMIT")]
-    Limit,
-    #[serde(rename="MARKET")]
-    Market,
-}
--- target/rust/binance_asyncapi/model/side.rs
+++
@@ -1,8 +0,0 @@
-// Side represents a Side model.
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-pub enum Side {
-    #[serde(rename="BUY")]
-    Buy,
-    #[serde(rename="SELL")]
-    Sell,
-}
