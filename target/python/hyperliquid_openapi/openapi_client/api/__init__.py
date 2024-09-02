641b0dcf098e837f5a539b00d6a2d31daf081894
Sho Kaneko <kanekoshoyu@gmail.com>
2024-09-02 10:12:44 +0200

    remove files other than generic binance code for easier navigation during dev

--- target/python/hyperliquid_openapi/openapi_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.exchange_api import ExchangeApi
-from openapi_client.api.info_api import InfoApi
-from openapi_client.api.websocket_api import WebsocketApi
-
